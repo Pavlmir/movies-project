3fd8a63cb700c922d78dfdf2aa306d21ebe6742a
Гоняев Павел Александрович [ДЗО] <PAGonyaev@sberbank.ru>
2023-04-10 22:56:10 +0300

    init

---
+++ src/components/Movie.jsx
@@ -0,0 +1,12 @@
+function Movie(props) {
+    const {
+        Title: title,
+        Year: year,
+        imdbID: id,
+        Type: type,
+        Poster: poster
+    } = props;
+
+}
+
+export {Movie}
