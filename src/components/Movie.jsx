ba13537df00dd03cc4b08ecf1aa2daffaf4e49ff
Гоняев Павел Александрович [ДЗО] <PAGonyaev@sberbank.ru>
2023-04-18 17:18:20 +0300

    ok

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,6 +7,21 @@ function Movie(props) {
         Poster: poster
     } = props;
 
+    return <div id={id} className="card movie">
+                <div className="card-image waves-effect waves-block waves-light">
+                    {
+                        poster === "N/A" ?
+                            <img className="activator" src={`https://placehold.co/300x450?text=${title}`} alt={title}/>        
+                            :
+                            <img className="activator" src={poster} alt={title}/>
+                    }
+                </div>
+                <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">{title}</span>
+                    <p>{year} <span className="right">{type}</span></p>
+                </div>
+           </div>
+
 }
 
 export {Movie}
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,39 @@
-function Main() {
-    return <main className='container content'>
-        Hello from Movies App
-    </main>
+import React from "react"
+import { Movies } from "../components/Movies"
+import { Search } from "../components/Search"
+import { Preloader } from "../components/Preloader"
+
+class Main extends React.Component {
+    state = {
+        movies: [],
+    }
+    
+    componentDidMount() {
+        fetch("https://www.omdbapi.com/?i=tt3896198&apikey=de4d7c23&s=matrix")
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+
+    }
+
+    searchMovies = (str) => {
+        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=de4d7c23&s=${str}`)
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}))
+    }
+
+    render(){
+         const {movies} = this.state;
+
+         return <main className='container content'>
+                    <Search searchMovies={this.searchMovies}/>
+                    {
+                        movies.length ? (
+                            <Movies movies={this.state.movies}/>
+                        ) : <Preloader/>
+                    }
+                    
+                </main>
+    }
 }
 
 export {Main}
